d35089f8212333f8dcc33e4e4ecd4d1028844729
Ujjwal Chourasia <123556163+02Ujjwal04@users.noreply.github.com>
2023-08-30 01:46:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let displayValue = "";
+
+function appendNumber(number) {
+  displayValue += number;
+  updateDisplay();
+}
+
+function appendOperator(operator) {
+  displayValue += " " + operator + " ";
+  updateDisplay();
+}
+
+function clearDisplay() {
+  displayValue = "";
+  updateDisplay();
+}
+
+function backspace() {
+  displayValue = displayValue.slice(0, -1);
+  updateDisplay();
+}
+
+function calculateResult() {
+  try {
+    displayValue = eval(displayValue).toString();
+  } catch (error) {
+    displayValue = "Error";
+  }
+  updateDisplay();
+}
+
+function updateDisplay() {
+  const display = document.getElementById("result");
+  display.value = displayValue;
+}
